79862faf7b8175e138a00470d4c4f11b6bcaa62b
Ananda Chanda <anandachanda08@gmail.com>
2024-10-27 19:21:32 +0530

    cors fix

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/Db");
 const authRoutes = require("./routes/AuthRoutes");
 const applicationRoutes = require("./routes/ApplicationRoutes");
@@ -8,10 +9,16 @@ const userRoutes = require("./routes/UserRoutes");
 const app = express();
 connectDB();
 
+// Enable CORS with more explicit options
+app.use(cors({
+    origin: "*", // Allows requests from all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/application", applicationRoutes);
-
 app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
